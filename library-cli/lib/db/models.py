e5d478b07cb77def86f4174bf5ac9123b3b98d99
David <DavidFriend86@hotmail.com>
2023-08-30 18:32:14 -0500

    book class + properties created

--- library-cli/lib/db/models.py
+++ library-cli/lib/db/models.py
@@ -1,3 +1,4 @@
+from statistics import mean
 class User:
     id = 1
     log =[]
@@ -8,6 +9,7 @@ class User:
         self._username = username
         self._password = password
         User.add_new_user(username,password)
+        User.append_to_log()
 
     def checkout(self,book):
         self.books.append(book.title)
@@ -17,11 +19,22 @@ class User:
         self.books.remove(book.title)
         book.restock()
 
+    @classmethod
+    def add_new_user(self,username, password):
+        self.users.update({username: password})
+    @classmethod
+    def append_to_log(self):
+        self.log.append(self.username)
+
     # id property methods
     def _get_id(self):
         return self._id
+
     def _set_id(self):
-        self._id += 1
+        if self._id == None:
+            self._id = 0
+        else:
+            self._id += 1
         return self._id
 
     # username methods
@@ -64,6 +77,88 @@ class User:
         fget = _get_password,
         fset = _set_password,
         doc = 'The password property'
+    )
+
+class Book:
+
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.stocked = True
+        self._scores = []
+
+    #id methods
+    def _get_id(self):
+        return self._id
+    def _set_id(self):
+        if self._id == None:
+            self._id = 0
+        else:
+            self._id += 1
+        return self._id
+
+    #owner methods
+    def _get_owner(self):
+        return self._owner
+    def _set_owner(self, o):
+        self._owner = o
+
+    #scores methods
+    def _get_scores(self):
+        return self._scores
+    def _set_scores(self, sc):
+        self._scores.append(sc)
+
+     #reviews methods
+    def _get_reviews(self):
+        return self._reviews
+    def _set_reviews(self,rev):
+        if rev == '' or rev == ' ':
+            raise AttributeError('Please input a review.')
+        else:
+            self._reviews.append(rev)
+
+    #rating methods
+    def _get_rating(self):
+        return self._rating
+    def _set_rating(self):
+        self._rating =int(mean(self.scores))
+
+
+    #id property
+    id = property(
+        fget = _get_id,
+        fset = _set_id,
+        doc = 'The id property'
+        )
+
+    #owner property
+    owner = property(
+        fget = _get_owner,
+        fset = _set_owner,
+        doc= 'The owner property'
+    )
+
+    #scores property
+    scores = property(
+        fget= _get_scores,
+        fset= _set_scores,
+        doc= "The scores property"
+    )
+
+    #reviews property
+    reviews = property(
+        fget= _get_reviews,
+        fset = _set_reviews,
+        doc = 'The reviews property'
+    )
+
+    #rating property
+    rating = property(
+        fget= _get_rating,
+        fset= _set_rating,
+        doc= 'The rating property'
     )
 
 
